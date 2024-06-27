46dfb0a0266bad0775dcd157a7c3051a15edcd83
wadeKings <worldkingscoding@gmail.com>
2024-06-27 21:04:06 +0800

    添加文章

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,9 @@
 import { defineUserConfig } from "vuepress";
+
 import theme from "./theme.js";
-import {hopeTheme} from "vuepress-theme-hope";
 
 export default defineUserConfig({
-  base: "/blog/",
+  base: "/",
   lang: "zh-CN",
   title: "柳衣白卿",
   description: "柳衣白卿",
